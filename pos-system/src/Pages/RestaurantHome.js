2bda53532e197e007d05b7ad369fb986a43a4171
Sarah-Ansari <sarahansari633@gmail.com>
2023-08-20 01:34:03 +0530

    HomePage

---
+++ pos-system/src/Pages/RestaurantHome.js
@@ -0,0 +1,19 @@
+import React from "react";
+import LayoutComponent from "../component/LayoutComponent";
+
+
+class RestHomePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (
+            <LayoutComponent>
+                
+            </LayoutComponent>
+          );
+    }
+}
+ 
+export default RestHomePage;
